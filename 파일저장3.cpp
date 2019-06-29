1be6c89d0a7f9f23211ffef244d2606c8c142764
andy7789 <52339673+andy7789@users.noreply.github.com>
2019-06-29 14:07:49 +0900

    Add files via upload

---
+++ 파일저장3.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+int main()
+{
+	string day, jamok, a;
+	
+	cout << "날짜: " << endl; cin >> day;
+	ofstream os{ "C:/temp/" + day + ".txt" };
+	cout << "제목: " << endl; cin >> jamok;
+	cout << "내용: " << endl; cin >> a;
+	
+	os << day << endl;
+	os << jamok << endl;
+	os << a << endl;
+	
+	return 0;
+}
